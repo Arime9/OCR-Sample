767dc48d42920b42f3aca006b10e0f444683bd61
m_aria <awo.j.xd@gmail.com>
2016-04-13 12:19:59 +0900

    UIとOCR処理

--- OCR-Sample/ViewController.m
+++ OCR-Sample/ViewController.m
@@ -14,14 +14,53 @@
 
 @implementation ViewController
 
+- (instancetype)init {
+    self = [super init];
+    if (self) [self initializator];
+    return self;
+}
+
+- (instancetype)initWithCoder:(NSCoder *)aDecoder {
+    self = [super initWithCoder:aDecoder];
+    if (self) [self initializator];
+    return self;
+}
+
+- (instancetype)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil {
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) [self initializator];
+    return self;
+}
+
+- (void)initializator {
+    // Initialization code
+}
+
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)viewWillAppear:(BOOL)animated {
+    [super viewWillAppear:animated];
+}
+
+- (void)viewDidAppear:(BOOL)animated {
+    [super viewDidAppear:animated];
+}
+
+- (void)viewWillDisappear:(BOOL)animated {
+    [super viewWillDisappear:animated];
+}
+
+- (void)viewDidDisappear:(BOOL)animated {
+    [super viewDidDisappear:animated];
 }
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+}
+
+- (void)dealloc {
 }
 
 @end
