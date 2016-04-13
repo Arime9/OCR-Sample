d181b729e82b7e6901fd46993cd38ea72d06e812
m_aria <awo.j.xd@gmail.com>
2016-04-13 12:16:23 +0900

    init Project.

---
+++ OCR-Sample/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  OCR-Sample
+//
+//  Created by Arai on 2016/04/13.
+//  Copyright © 2016年 masato_arai. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ OCR-Sample/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  OCR-Sample
+//
+//  Created by Arai on 2016/04/13.
+//  Copyright © 2016年 masato_arai. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
